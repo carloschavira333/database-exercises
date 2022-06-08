e18621e68e3c1f8de44aacb4cdb0f9bb5ae2d2fe
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-06-08 15:32:15 -0500

    completed Group By ex.

---
+++ group_by_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY last_name;
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' GROUP BY first_name, last_name;
+
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+SELECT COUNT(*), last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'GROUP BY last_name;
+
+SELECT COUNT(*), gender FROM employees WHERE first_name IN ('Irena' , 'Vidya', 'Maya') GROUP BY gender;
+
